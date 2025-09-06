f63b3c4474f656e9c59b01fde2154a04c7b191a7
Thanh Do <thanh@pullable.com>
2025-09-06 14:40:25 +0700

    Updated resume url

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
     "A passionate Junior Fullstack Developer 🚀, coming back to tech after a career break. I enjoy building clean, responsive web apps with React, Spring Boot, and PostgreSQL."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1XdizKVAE2VZbnarCI0a5axuRXzqu0H1p/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1bexxjhSVTNP2da4jDdczUrN1uBRYNiJf/view?usp=drive_link", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
